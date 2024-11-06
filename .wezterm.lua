c96ddffe1847678029ba3fa5629a4a3ebbce2ea6
Caio Fernandes <caiovf13@gmail.com>
2024-11-06 17:07:23 -0300

    Add smart-split integration

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,55 @@
 local wezterm = require("wezterm")
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 local config = wezterm.config_builder()
+local w = require("wezterm")
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
+-- you have to use this instead, but note that this will not work
+-- in all cases (e.g. over an SSH connection). Also note that
+-- `pane:get_foreground_process_name()` can have high and highly variable
+-- latency, so the other implementation of `is_vim()` will be more
+-- performant as well.
+local function is_vim(pane)
+	-- This gsub is equivalent to POSIX basename(3)
+	-- Given "/foo/bar" returns "bar"
+	-- Given "c:\\foo\\bar" returns "bar"
+	local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
+	return process_name == "nvim" or process_name == "vim"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = w.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
 
 config.enable_tab_bar = true
 
@@ -12,6 +61,15 @@ config.send_composed_key_when_right_alt_is_pressed = false
 config.send_composed_key_when_left_alt_is_pressed = true
 
 config.keys = {
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 	--Fullscreen
 	{
 		key = "Enter",
@@ -45,27 +103,27 @@ config.keys = {
 		mods = "CMD|SHIFT",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
-	-- Windows Movement
-	{
-		key = "l",
-		mods = "OPT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "k",
-		mods = "OPT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "OPT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "h",
-		mods = "OPT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
+	-- -- Windows Movement
+	-- {
+	-- 	key = "l",
+	-- 	mods = "OPT",
+	-- 	action = wezterm.action.ActivatePaneDirection("Right"),
+	-- },
+	-- {
+	-- 	key = "k",
+	-- 	mods = "OPT",
+	-- 	action = wezterm.action.ActivatePaneDirection("Up"),
+	-- },
+	-- {
+	-- 	key = "j",
+	-- 	mods = "OPT",
+	-- 	action = wezterm.action.ActivatePaneDirection("Down"),
+	-- },
+	-- {
+	-- 	key = "h",
+	-- 	mods = "OPT",
+	-- 	action = wezterm.action.ActivatePaneDirection("Left"),
+	-- },
 	-- Bind CopyMode to CMD + x
 	{
 		key = "x",
