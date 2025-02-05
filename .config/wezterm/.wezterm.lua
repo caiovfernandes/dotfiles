43d36c062c19195258fd062a5a48438e3fc805f1
Caio Fernandes <caiovf13@gmail.com>
2025-02-04 21:28:10 -0300

    Add wezterm cyberdream theme

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 local config = wezterm.config_builder()
 local w = require("wezterm")
 local io = require("io")
@@ -55,24 +54,6 @@ local function split_nav(resize_or_move, key)
 	}
 end
 
-config.enable_tab_bar = true
-
-config.font = wezterm.font({
-	family = "JetBrains Mono",
-})
-config.native_macos_fullscreen_mode = true
-config.max_fps = 120
-config.animation_fps = 120
-config.send_composed_key_when_right_alt_is_pressed = false
-config.send_composed_key_when_left_alt_is_pressed = true
-config.mouse_wheel_scrolls_tabs = true
-config.tab_max_width = 50
-local gpus = wezterm.gui.enumerate_gpus()
-
-config.webgpu_preferred_adapter = gpus[1]
-config.front_end = "WebGpu"
-config.dpi = 144
-config.custom_block_glyphs = false
 config.keys = {
 
 	split_nav("move", "h"),
@@ -215,24 +196,39 @@ config.color_scheme = "Duckbones"
 -- config.color_scheme = "Material Vivid (base16)"
 -- config.color_scheme = "midnight-in-mojave"
 -- config.color_scheme = "Mikazuki (terminal.sexy)"
-config.font_size = 16
-config.line_height = 1.2
-config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.75
 config.macos_window_background_blur = 50
 config.use_fancy_tab_bar = false
-tabline.setup()
+
 config.notification_handling = "AlwaysShow"
-config.anti_alias_custom_block_glyphs = true
+-- config.anti_alias_custom_block_glyphs = true
 config.audible_bell = "SystemBeep"
-config.bold_brightens_ansi_colors = true
+config.dpi = 108
 config.font_shaper = "Harfbuzz"
+config.font_size = 22
+config.line_height = 1.3
+
+config.font = wezterm.font({
+	family = "Monaspace Neon",
+})
+config.color_scheme = "Ciberdream"
+
+config.enable_tab_bar = false
+
+config.native_macos_fullscreen_mode = true
+config.max_fps = 120
+config.animation_fps = 120
+config.send_composed_key_when_right_alt_is_pressed = false
+config.send_composed_key_when_left_alt_is_pressed = true
+config.tab_max_width = 50
+config.front_end = "WebGpu"
 
 config.set_environment_variables = {
 	SHELL = "/bin/zsh",
 }
+
 wezterm.on("trigger-vim-with-scrollback", function(window, pane)
 	-- Retrieve the text from the pane
 	local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
